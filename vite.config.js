c773cffad4a47a175b5008a20c708ea95101fbfd
Dominic <dominic.kirste@gmail.com>
2022-10-11 18:48:43 +0200

    modified vite config - added a src folder to hold all pages

--- vite.config.js
+++ vite.config.js
@@ -2,13 +2,17 @@ import { defineConfig } from 'vite';
 import { resolve } from 'path';
 
 export default defineConfig({
+  root: 'src',
   // https://vitejs.dev/guide/build.html#multi-page-app
   build: {
     rollupOptions: {
       input: {
-        main: resolve(__dirname, 'index.html'),
-        _003: resolve(__dirname, 'lesson/003/index.html'),
-        _005: resolve(__dirname, 'lesson/005/index.html'),
+        main: resolve(__dirname, 'src/index.html'),
+        _003: resolve(__dirname, 'src/lesson/003/index.html'),
+        _005: resolve(__dirname, 'src/lesson/005/index.html'),
+      },
+      output: {
+        dir: 'dist',
       }
     }
   }

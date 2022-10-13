fd87038e68bc12727b2d6cece8a4a24e2e093de6
Dominic <dominic.kirste@gmail.com>
2022-10-13 16:44:11 +0200

    toon shader project - added comments - deleted unnecessary code - created a group for the spheres and let the cam look at it

--- src/lesson/001/script.js
+++ src/lesson/001/script.js
@@ -4,19 +4,15 @@ import fiveToneUrl from './tones/fiveTone.jpeg'
 import * as THREE from 'three';
 import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls.js';
 
-const cursor = {
-  x: 0,
-  y: 0
-}
-
-window.addEventListener('mousemove', (event) => {
-  cursor.x = event.clientX / window.innerWidth - 0.5;
-  cursor.y = -(event.clientY / window.innerWidth - 0.5);
-});
-
+// Scene
+// Scenes allow you to set up what and where is to be rendered by three.js. 
+// This is where you place objects, lights and cameras.
 const scene = new THREE.Scene();
 
-const fov = 75;
+// PerspectiveCamera
+// Camera that uses perspective projection.
+// https://en.wikipedia.org/wiki/Perspective_(graphical)
+const fov = 75; // field of view
 const aspect = window.innerWidth / window.innerHeight;
 const near = 1;
 const far = 1000;
@@ -24,23 +20,34 @@ const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
 camera.position.set(0, 0, 500);
 scene.add(camera);
 
+// PointLight
+// A light that gets emitted from a single point in all directions. 
+// A common use case for this is to replicate the light emitted from a bare lightbulb.
 const color = 0xffffff; // default value
 const intensity = 1; // default value
 const pointlight = new THREE.PointLight(color, intensity);
 pointlight.position.set(200, 200, 200);
 scene.add(pointlight);
 
+// SphereGeometry
+// A class for generating sphere geometries.
 const pearlRadius = 100;
-const pearlWidthSegments = 64;
-const pearlHeightSegments = 64;
+const pearlWidthSegments = 64; // number of horizontal segments. Minimum value is 3, and the default is 32.
+const pearlHeightSegments = 64; // number of vertical segments. Minimum value is 2, and the default is 16.
 const pearlGeometry = new THREE.SphereGeometry(pearlRadius, pearlWidthSegments, pearlHeightSegments);
 
+// TextureLoader
+// Class for loading a texture. This uses the ImageLoader internally for loading files.
 const pearlTextureLoader = new THREE.TextureLoader();
 
 const threeTone = pearlTextureLoader.load(threeToneUrl);
 threeTone.minFilter = THREE.NearestFilter;
 threeTone.magFilter = THREE.NearestFilter;
 
+// MeshToonMaterial
+// A material implementing toon shading.
+// https://en.wikipedia.org/wiki/Cel_shading
+// https://threejs.org/docs/?q=MeshToonMaterial#api/en/materials/MeshToonMaterial
 const pearlMaterialThreeTone = new THREE.MeshToonMaterial({
   color: 0x049ef4,
   gradientMap: threeTone,
@@ -48,7 +55,6 @@ const pearlMaterialThreeTone = new THREE.MeshToonMaterial({
 
 const pearl1 = new THREE.Mesh(pearlGeometry, pearlMaterialThreeTone);
 pearl1.position.y = 120;
-scene.add(pearl1);
 
 const fiveTone = pearlTextureLoader.load(fiveToneUrl);
 fiveTone.minFilter = THREE.NearestFilter;
@@ -61,8 +67,13 @@ const pearlMaterialFiveTone = new THREE.MeshToonMaterial({
 
 const pearl2 = new THREE.Mesh(pearlGeometry, pearlMaterialFiveTone);
 pearl2.position.y = -120;
-scene.add(pearl2);
 
+const pearlGroup = new THREE.Group();
+pearlGroup.add(pearl1);
+pearlGroup.add(pearl2);
+scene.add(pearlGroup);
+
+camera.lookAt(pearlGroup.position);
 
 const renderer = new THREE.WebGLRenderer({
   canvas: document.querySelector('.webgl'),

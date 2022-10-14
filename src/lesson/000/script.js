1b5727d37b5160d165d90866b18e529333eb19c9
Dominic <dominic.kirste@gmail.com>
2022-10-14 20:57:34 +0200

    added data object

--- src/lesson/000/script.js
+++ src/lesson/000/script.js
@@ -3,6 +3,33 @@ import * as THREE from 'three';
 import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls.js';
 import * as dat from 'dat.gui';
 
+const data = {
+
+  camera: {
+    fov: 75,
+    aspect: window.innerWidth / window.innerHeight,
+    near: 1,
+    far: 1000,
+  },
+
+  renderer: {
+    maxPixelRatio: 2,
+    updateStyle: false,
+  },
+
+  pointlight: {
+    color: 0xffffff,
+    intensity: 1,
+  },
+
+  cube: {
+    material: {
+      color: 0x00ff00,
+    },
+  },
+
+}
+
 const gui = new dat.GUI();
 
 /**
@@ -23,15 +50,31 @@ const scene = new THREE.Scene();
  * PerspectiveCamera( fov : Number, aspect : Number, near : Number, far : Number )
  */
 const camera = new THREE.PerspectiveCamera(
-  75,
-  window.innerWidth / window.innerHeight,
-  0.1,
-  8,
+  data.camera.fov,
+  data.camera.aspect,
+  data.camera.near,
+  data.camera.far,
 );
 camera.position.x = 0;
 camera.position.y = 0;
 camera.position.z = 2;
 scene.add(camera);
+
+/**
+ * PointLight
+ * A light that gets emitted from a single point in all directions.
+ * A common use case for this is to replicate the light emitted from a bare lightbulb.
+ * https://threejs.org/docs/?q=PointLight#api/en/lights/PointLight
+ * 
+ * PointLight( color : Integer, intensity : Float, distance : Number, decay : Float )
+ */
+const pointlight = new THREE.PointLight(
+  data.pointlight.color, 
+  data.pointlight.intensity,
+);
+
+pointlight.position.set(200, 200, 200);
+scene.add(pointlight);
 
 /**
  * Mesh
@@ -43,22 +86,19 @@ scene.add(camera);
  * material — (optional) a single or an array of Material. Default is a new MeshBasicMaterial
  */
 const cube = new THREE.Mesh();
+
 cube.geometry = new THREE.BoxGeometry(0.4, 0.4, 0.4);
 cube.material = new THREE.MeshBasicMaterial({
-  color: 0x00ff00,
+  color: data.cube.material.color,
   wireframe: true,
 });
+
 cube.position.set(0, 0, 0);
 scene.add(cube);
 
-// Debug
-const palette = {
-  color: 0x00ff00,
-};
-
 gui
-  .addColor(palette, 'color')
-  .onChange(() => cube.material.color.set(palette.color))
+  .addColor(data.cube.material, 'color')
+  .onChange(() => cube.material.color.set(data.cube.material.color))
   ;
 
 gui
@@ -105,23 +145,27 @@ const renderer = new THREE.WebGLRenderer({
   canvas: document.querySelector('.webgl'),
   antialias: true,
 });
-renderer.setSize(window.innerWidth, window.innerHeight, false);
-renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+
+renderer.setSize(window.innerWidth, window.innerHeight, data.renderer.updateStyle);
+
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, data.renderer.maxPixelRatio));
+
 renderer.setAnimationLoop(() => {
-  controls.update();
+  orbitControls.update();
   renderer.render(scene, camera);
 });
+
 renderer.render(scene, camera);
 
-const controls = new OrbitControls(camera, renderer.domElement);
-controls.enableDamping = true;
+const orbitControls = new OrbitControls(camera, renderer.domElement);
+orbitControls.enableDamping = true;
 
 function onWindowResize() {
   const width = window.innerWidth;
   const height = window.innerHeight;
 
-  renderer.setSize(width, height, false);
-  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+  renderer.setSize(width, height, data.renderer.updateStyle);
+  renderer.setPixelRatio(Math.min(window.devicePixelRatio, data.renderer.maxPixelRatio));
 
   camera.aspect = width / height;
   camera.updateProjectionMatrix();
